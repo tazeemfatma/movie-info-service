30c61a8922efe25d21fcef66b682dade4afeca11
Tazeem Fatma <Tazeem.Fatma@BRILLIO.COM>
2023-10-04 17:32:50 +0530

    Initial commit

---
+++ src/main/java/com/example/movieinfoservice/model/Movie.java
@@ -0,0 +1,11 @@
+package com.example.movieinfoservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Movie {
+    private String movieId;
+    private String movieName;
+}
---
+++ src/main/java/com/example/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,16 @@
+package com.example.movieinfoservice.resources;
+
+import com.example.movieinfoservice.model.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovie(@PathVariable("movieId") String movieId){
+        return new Movie(movieId, "Transformers");
+    }
+}
